b4e1ae5795c0cf2a38f2db4278897ceade997c41
Vic <patriciu_victor@hotmail.com>
2022-07-06 17:01:24 +0530

    added Cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
   graphql: {
     enabled: true,
     config: {
@@ -8,6 +8,21 @@ module.exports = () => ({
       apolloServer: {
         tracing: true,
         introspection: true,
+      },
+    },
+  },
+
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
   },
