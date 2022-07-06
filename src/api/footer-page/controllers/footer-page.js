977f9a6ee1f50bb02923c19b51b41e6d354bf5bd
Vic <patriciu_victor@hotmail.com>
2022-07-06 20:57:39 +0530

    fixes and improvements

---
+++ src/api/footer-page/controllers/footer-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  footer-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::footer-page.footer-page');
---
+++ src/api/footer-page/routes/footer-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * footer-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::footer-page.footer-page');
